1721cf7f9cecab44a9fc4996088d7ea7c1f387d0
danysai <48719605+danysainero@users.noreply.github.com>
2021-02-15 21:56:41 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+'use-strict';
+
+import { Router as router } from './routing/router.js';
+
+
+router(['home', 'action', 'graphics'], 'home', 'router-outlet');
---
+++ routing/router.js
@@ -0,0 +1,25 @@
+const loadPage = (pageName, routerOutletId) => {
+    const element = document.getElementById(routerOutletId);
+    const request = new XMLHttpRequest();
+    request.onload = () => replaceInnerHtmlWithNewPage(request, element);
+    request.open("GET", `pages/${pageName}.html`, true);
+    request.send();
+};
+ 
+ const replaceInnerHtmlWithNewPage = (response, element) => {
+    if (response.readyState === 4 && response.status === 200) {
+        element.innerHTML = response.responseText;
+    }
+};
+
+const getNewRoute = (routes, defaultRoute) =>
+    routes.find(route => window.location.hash.replace("#", "") === route) ||
+    defaultRoute;
+
+    export const Router = (routes, defaultRoute, routerOutletId) => {
+  
+    loadPage(getNewRoute(routes, defaultRoute), routerOutletId);
+    window.addEventListener("hashchange", () =>
+        loadPage(getNewRoute(routes, defaultRoute), routerOutletId)
+    );
+};
