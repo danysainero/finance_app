a487e3bb46c3541e67f436f80e154fe552f43eb0
danysai <48719605+danysainero@users.noreply.github.com>
2021-02-16 19:53:07 +0100

    add header, navbar and menu

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-'use-strict';
-
 import { Router as router } from './routing/router.js';
 
-
 router(['home', 'action', 'graphics'], 'home', 'router-outlet');
+
+const navbarBurger = document.querySelector('.navbar-burger');
+const menuElements = document.querySelectorAll('.navbar-item');
+
+navbarBurger.addEventListener('click', function () {
+    const navbarBurgerMenu = document.querySelector('#navbar-menu');
+    navbarBurgerMenu.classList.toggle('is-active');
+});
+
+menuElements.forEach((el) => {
+    el.addEventListener('click', function () {
+        const navbarBurgerMenu = document.querySelector('#navbar-menu');
+        navbarBurgerMenu.classList.toggle('is-active');
+    })
+});
