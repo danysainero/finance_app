d77fe57581ebec9df8c612b4643e0dd600733227
danysai <48719605+danysainero@users.noreply.github.com>
2021-03-05 18:57:18 +0100

    add cancel button function

--- pages/action/action.js
+++ pages/action/action.js
@@ -5,7 +5,7 @@ export const actionInit = () => {
 
     const periodicity = document.querySelectorAll('input[name="periodicity"]');
     const endDate = document.querySelector('[name="endDateContainer"]');
-   
+
     periodicity.forEach(el => {
         el.addEventListener('click', () => {
             endDate.classList.toggle('hidden')
@@ -13,23 +13,35 @@ export const actionInit = () => {
     })
 
     formSubmit.addEventListener('click', async () => {
-
+        console.log('submit');
         let tag = document.getElementById('name').value
         let amount = document.getElementById('price').value
         let type = document.querySelector('input[name="movementType"]:checked').value
         let recurrent = document.querySelector('input[name="periodicity"]:checked').value
         let startDate = document.getElementById('startDate').value
         let endDate = document.getElementById('endDate').value
-        
+
 
         startDate = new Date(startDate);
-        startDate.setDate( startDate.getDate() - 1 );
+        startDate.setDate(startDate.getDate() - 1);
         endDate = new Date(endDate);
-        endDate.setDate( endDate.getDate() + 1 );
+        endDate.setDate(endDate.getDate() + 1);
 
-         await MovementsProxy.createMovement({tag, amount, type, recurrent, startDate, endDate });
+        await MovementsProxy.createMovement({
+            tag,
+            amount,
+            type,
+            recurrent,
+            startDate,
+            endDate
+        });
 
         location.replace("http://localhost:5500/#home");
     });
+
+    cancel.addEventListener('click', async () => {
+        location.replace("http://localhost:5500/#home");
+    });
+
 
 }
